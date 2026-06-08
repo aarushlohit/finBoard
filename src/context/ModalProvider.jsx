cdf832c3688b5640123ab175249882ea4252f6ff
dravidpa7 <dravidpa20@gmail.com>
2026-06-08 05:47:23 +0530

    fix: add missing import for ModalContext in ModalProvider

--- src/context/ModalProvider.jsx
+++ src/context/ModalProvider.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ModalContext } from './ModalContext';
 
 export const ModalProvider = ({ children }) => {
   const [modalConfig, setModalConfig] = useState(null);
